515173f5916c6b32a95cbfc6ad1a385ae6fc4178
InDev-tech <ivannatalia07082009@gmail.com>
2026-04-13 21:31:27 +0500

    new file:   Leetcode/easy/python/intersection_of_two_arrays.py

---
+++ Leetcode/easy/python/intersection_of_two_arrays.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        return list(set(nums1).intersection(set(nums2)))
+
+result = Solution()
+print(result.intersection([1,2,2,1], [2,2]))
